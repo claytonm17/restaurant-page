19447bc35aba9f645146ba237055b0a1dd010741
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-12-12 09:37:48 -0500

    feat: build header from javascript

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("// Creates the top bar of webpage\nfunction createHeader(text) {\n    const header = document.createElement('header');\n    header.setAttribute('class', 'heading');\n    \n    // Restaurant Title\n    const title = document.createElement('h1');\n    title.textContent = text;\n    header.appendChild(title);\n\n    // Nav bar creation\n    const nav = document.createElement('nav');\n    nav.setAttribute('class', 'navbar');\n    \n    nav.appendChild(createButton('home-button', 'Home'));\n    nav.appendChild(createButton('menu-button', 'Menu'));\n    nav.appendChild(createButton('contact-button', 'Contact'));\n\n\n    header.appendChild(nav);\n    return header;\n}\n\n// Navbar creation\nfunction createButton(selector, button) {\n    const newButton = document.createElement('button');\n    newButton.setAttribute('class', selector);\n    newButton.textContent = button;\n    return newButton;\n}\n\nconst contentContainer = document.querySelector('#content');\ncontentContainer.appendChild(createHeader('Cat fish'))\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
